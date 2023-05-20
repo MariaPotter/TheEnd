c503ad74afaf8a7bf35a15111cb57a56f4eb728d
Mary Potter <mari.gorshkova.1147@mail.ru>
2023-05-20 23:00:33 +0300

    Начало чего-то большого и страшного

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <cmath>
-#include <cstdio>
 #include <fftw3.h>        //БДПФ
 #include <iostream>
-#include <stdlib.h>
 
 using namespace std;
 
-unsigned int N = 5;
-
 double f (double x, double y, double z);
 double u (double x, double y, double z);
-void        change_F (double* G, double* F);
-double norm (double* G, double* U);
+void   change_F (double* G, double* F, unsigned int N);
+double norm (double* G, double* U, unsigned int N);
 
 int main()
 {
-    unsigned int m;
-    int          n;
-    double* F;
-    double* U;
-    double* G;
-    fftw_plan p;
+    unsigned int N;        // количество точек апроксимации
+    unsigned int m;        // временное хранение, площадь сетки
+    int n;                 // размерность вектора
 
+    double* F;        // численное решение
+    double* U;        // аналитическое решение
+    double* G;        // временное хранение
+
+    fftw_plan p;        // настройки ДБПФ
+
+    N = 5;
     n = static_cast<int> (N - 1);
     m = static_cast<unsigned int> (pow (n, 3));
 
@@ -46,13 +46,13 @@ int main()
     }
 
     p = fftw_plan_r2r_3d (n, n, n, F, G, FFTW_RODFT00, FFTW_RODFT00,
-                           FFTW_RODFT00, FFTW_ESTIMATE);
+                          FFTW_RODFT00, FFTW_ESTIMATE);
 
-    fftw_execute (p);
-    change_F (G, F);
-    fftw_execute (p);
+    fftw_execute (p);          // выполнение прямого ДБПФ
+    change_F (G, F, N);        // решение ЛАУ
+    fftw_execute (p);          // выполнение обратного ДБПФ
 
-    printf ("N = %u,\tnorm = %f\n", N, norm (G, U));
+    cout << "N = " << N << ",\tnorm = " << norm (G, U, N) << endl;
 
     fftw_destroy_plan (p);
 
@@ -68,18 +68,18 @@ double f (double x, double y, double z)
     return 3 * sin (x) * sin (y) * sin (z);
 }
 
-double u (double x, double y, double z)
-{
-    return sin (x) * sin (y) * sin (z);
-}
+double u (double x, double y, double z) { return sin (x) * sin (y) * sin (z); }
 
-void change_F (double* G, double* F)
+void change_F (double* G, double* F, unsigned int N)
 {
     unsigned int m;
-    double* c = new double[N - 1];
-    m              = static_cast<unsigned int> (pow (N - 1, 2));
+    double*      c;
+
+    m = static_cast<unsigned int> (pow (N - 1, 2));
+    c = new double[N - 1];
+
     for (unsigned int i = 1; i < N; i++)
-        c[i - 1] = pow (sin (i * M_PI / (2 * N)), 2);
+        c[i - 1] = pow (2 * N * sin (i * M_PI / (2 * N)) / M_PI, 2);
 
     for (unsigned int i = 0; i < N - 1; i++)
     {
@@ -87,17 +87,19 @@ void change_F (double* G, double* F)
         {
             for (unsigned int k = 0; k < N - 1; k++)
                 F[i * m + j * (N - 1) + k] =
-                    G[i * m + j * (N - 1) + k] * pow (M_PI, 2) /
-                    (pow (2 * N, 5) * (c[i] + c[j] + c[k]));
+                    G[i * m + j * (N - 1) + k] /
+                    (pow (2 * N, 3) * (c[i] + c[j] + c[k]));
         }
     }
 }
 
-double norm (double* G, double* U)
+double norm (double* G, double* U, unsigned int N)
 {
-    double norm = static_cast<double> (0);
+    double norm;
+    norm = static_cast<double> (0);
+
     for (unsigned int i = 0; i < pow (N - 1, 3); i++)
         norm += pow (U[i] - G[i], 2);
-    norm = sqrt (norm / pow (N + 1, 3));
-    return norm;
+
+    return sqrt (norm / pow (N + 1, 3));
 }
